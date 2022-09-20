ac5d6ad73f4675320c5780a9d72039ae5a3ba15a
Vipin Sharma <vipin.sharma2024@gmail.com>
2022-09-20 10:18:39 +0400

    code refactoring

--- src/runService.py
+++ src/runService.py
@@ -1,43 +1,24 @@
-import os
 import logging
-import json
-from datetime import datetime
+import utils
 
-from mvAcquire.mvDeviceUtils import findActiveDevices
+from mvAcquire import mvDeviceUtils
 
-
-def readConfig(filename):
-    with open(filename, "r") as file:
-        config = json.load(file)
-    return config
-
-
-def getDateTimeRecordingDir(opts):
-    next_hour = str((datetime.now().hour + 1) % 24).zfill(2)
-    recording_dir = os.path.join(
-        opts["dirs"]["recdir"],
-        datetime.now().strftime("%Y-%m-%0d/%H") + "H-" + str(next_hour) + "H",
-    )
-    return recording_dir
-
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    opts = readConfig("../cfg/AppConfig.json")
-    recording_dir = getDateTimeRecordingDir(opts)
+    opts = utils.readConfig("../cfg/AppConfig.json")
+    recordingDir, logDir = utils.setupDirs(opts)
+    utils.setupLogging()
 
-    os.makedirs(opts["dirs"]["logdir"], exist_ok=True)
-    # os.makedirs(recording_dir, exist_ok=True)
+    logger.info("Recorded images will be saved at path " + recordingDir)
 
-    logging.basicConfig(
-        level=logging.DEBUG,
-        filename=os.path.join(
-            opts["dirs"]["logdir"],
-            "logs_{}.log".format(datetime.now().strftime("%Y-%m-%d_%H-%M-%S")),
-        ),
-        filemode="w",
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    )
+    mvDeviceUtils.findActiveDevices()
 
-    logging.info("Recorded images will be saved at path " + recording_dir)
+    # TODO: Add code to find camera of given family and initialize it
+    mvDeviceUtils.initCamera(opts["camera"]["family"])
 
-    findActiveDevices()
+    # TODO: Implement this function
+    # mvDeviceUtils.loadDeviceSettingsFromXML(opts["camera"]["config"])
+    # initPubSub() # TODO: Implement this function to initialize MQTT pub/sub
+
+    mvDeviceUtils.initRecording(recordingDir)
---
+++ src/utils.py
@@ -0,0 +1,41 @@
+import os
+import json
+from datetime import datetime
+import logging
+
+
+def readConfig(filename):
+    with open(filename, "r") as file:
+        config = json.load(file)
+    return config
+
+
+def getDateTimeRecordingDir(opts):
+    next_hour = str((datetime.now().hour + 1) % 24).zfill(2)
+    recording_dir = os.path.join(
+        opts["dirs"]["recdir"],
+        datetime.now().strftime("%Y-%m-%0d/%H") + "H-" + str(next_hour) + "H",
+    )
+    return recording_dir
+
+
+def setupDirs(opts):
+    recordingDir = getDateTimeRecordingDir(opts)
+    logDir = opts["dirs"]["logdir"]
+
+    # os.makedirs(recordingDir, exist_ok=True)
+    os.makedirs(logDir, exist_ok=True)
+
+    return recordingDir, logDir
+
+
+def setupLogging(logDir):
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename=os.path.join(
+            logDir,
+            "logs_{}.log".format(datetime.now().strftime("%Y-%m-%d_%H-%M-%S")),
+        ),
+        filemode="w",
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )

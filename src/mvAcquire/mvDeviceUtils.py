d4efe4fd749dde36c761c16e3e7e70503000eeea
Vipin Sharma <vipin.sharma2024@gmail.com>
2022-09-20 11:29:48 +0400

    updated code to load XML settings, need to verify

--- src/mvAcquire/mvDeviceUtils.py
+++ src/mvAcquire/mvDeviceUtils.py
@@ -11,6 +11,27 @@ deviceManager = acquire.DeviceManager()
 logger = logging.getLogger(__name__)
 
 
+def supportsValue(prop, value):
+    if prop.hasDict:
+        validValues = []
+        prop.getTranslationDictValues(validValues)
+        return value in validValues
+
+    if prop.hasMinValue and prop.getMinValue() > value:
+        return False
+
+    if prop.hasMaxValue and prop.getMaxValue() < value:
+        return False
+
+    return True
+
+
+def conditionalSetProperty(prop, value):
+    if prop.isValid and prop.isWriteable and supportsValue(prop, value):
+        prop.write(value)
+        logger.info("Property '%s' set to '%s'." % (prop.name(), prop.readS()))
+
+
 def getConnectedDeviceInfo():
     connectedDeviceInfo = ""
     for i in range(deviceManager.deviceCount()):
@@ -18,9 +39,18 @@ def getConnectedDeviceInfo():
         currDeviceInfo = ""
         currDeviceInfo = "[" + str(i) + "]: " + pDev.serial.read() \
             + "(" + pDev.product.read() + ", " + pDev.family.read()
+
+        if pDev.interfaceLayout.isValid:
+            conditionalSetProperty(pDev.interfaceLayout, acquire.dilGenICam)
+            currDeviceInfo += ", interface layout: " \
+                + pDev.interfaceLayout.readS()
+
         if pDev.acquisitionStartStopBehaviour.isValid:
+            conditionalSetProperty(pDev.acquisitionStartStopBehaviour,
+                                    acquire.assbUser)
             currDeviceInfo += ", acquisition start/stop behaviour: " \
                 + pDev.acquisitionStartStopBehaviour.readS()
+
         if pDev.isInUse:
             currDeviceInfo += ", !!!ALREADY IN USE!!!"
         currDeviceInfo += ")\n"
@@ -37,3 +67,29 @@ def findActiveDevices():
     else:
         logger.info("Found %d device(s)" % deviceManager.deviceCount())
         logger.info(getConnectedDeviceInfo())
+
+
+def loadDeviceSettings(fInterface, cameraSettingFile):
+    try:
+        fInterface.loadSetting(cameraSettingFile)
+    except Exception as e:
+        logger.error("Error loading camera settings file: %s" % e)
+        logger.error("Continuing without loading settings file")
+    else:
+        logger.info("Camera settings loaded from file: %s" % cameraSettingFile)
+
+
+def initCamera(cameraFamily, cameraSettingsFile):
+    cameraObject = deviceManager.getDeviceByFamily(cameraFamily)
+
+    if cameraObject is None:
+        logger.error("No camera belonging to family %s found" % cameraFamily)
+        logger.info("Re-initializing camera search")
+        findActiveDevices()
+        initCamera(cameraFamily, cameraSettingsFile)
+    else:
+        logger.info("Camera belonging to family %s found: %s" % 
+                        (cameraFamily, cameraObject.product.read()))
+        fInterface = acquire.FunctionInterface(cameraObject)
+        loadDeviceSettings(fInterface, cameraSettingsFile)
+        return cameraObject
--- src/runService.py
+++ src/runService.py
@@ -8,17 +8,14 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     opts = utils.readConfig("../cfg/AppConfig.json")
     recordingDir, logDir = utils.setupDirs(opts)
-    utils.setupLogging()
+    utils.setupLogging(logDir)
 
     logger.info("Recorded images will be saved at path " + recordingDir)
 
     mvDeviceUtils.findActiveDevices()
+    mvDeviceUtils.initCamera(opts["camera"]["family"], opts["camera"]["config"])
 
-    # TODO: Add code to find camera of given family and initialize it
-    mvDeviceUtils.initCamera(opts["camera"]["family"])
-
-    # TODO: Implement this function
-    # mvDeviceUtils.loadDeviceSettingsFromXML(opts["camera"]["config"])
-    # initPubSub() # TODO: Implement this function to initialize MQTT pub/sub
+    # TODO: Implement this function to initialize MQTT pub/sub
+    # initPubSub()
 
     mvDeviceUtils.initRecording(recordingDir)
